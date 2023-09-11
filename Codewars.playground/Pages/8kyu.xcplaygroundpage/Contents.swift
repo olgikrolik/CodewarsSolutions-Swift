07568a7e22a844353b2d662832fdb0515112afb1
Olga Królikowska <olga.szydlowska2@gmail.com>
2023-09-11 14:24:01 +0200

    Complete challenge 8kyu

--- Codewars.playground/Pages/8kyu.xcplaygroundpage/Contents.swift
+++ Codewars.playground/Pages/8kyu.xcplaygroundpage/Contents.swift
@@ -1,3 +1,17 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Challenge
+// Clock shows h hours, m minutes and s seconds after midnight.
+//Your task is to write a function which returns the time since midnight in milliseconds.
+
+func past(_ h: Int, _ m: Int, _ s: Int) -> Int {
+    
+   let hoursToMilliseconds = h * 3600000
+   let minutesToMilliseconds = m * 60000
+   let secondsToMilliseconds = s * 1000
+
+   return hoursToMilliseconds + minutesToMilliseconds + secondsToMilliseconds
+}
+
+
+past(2, 30, 22)
