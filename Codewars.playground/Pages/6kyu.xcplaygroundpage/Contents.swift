64e750cdf6d80f2a5f6d4609e46b2da19302caba
Olga Królikowska <olga.szydlowska2@gmail.com>
2023-09-11 17:48:27 +0200

    Complete challenge 6kyu

--- Codewars.playground/Pages/6kyu.xcplaygroundpage/Contents.swift
+++ Codewars.playground/Pages/6kyu.xcplaygroundpage/Contents.swift
@@ -1,7 +1,26 @@
-//: [Previous](@previous)
 
 import Foundation
 
-var greeting = "Hello, playground"
+//Chellange 1
+//Given an array of integers, find the one that appears an odd number of times.
+//There will always be only one integer that appears an odd number of times.
 
-//: [Next](@next)
+findIt([1, 1, 0, 3, 3, 5, 5, 1, 1])
+
+func findIt(_ seq: [Int]) -> Int {
+    var dictionary = [Int: Int]()
+    
+    for integer in seq {
+        let value = dictionary[integer]
+        if value == nil {
+            dictionary[integer] = 1
+        } else {
+            dictionary[integer]! += 1
+        }
+    }
+    
+    let selectedDictionary = dictionary.filter { $0.value % 2 != 0 }
+    let selectedKeys = selectedDictionary.map { $0.key }
+    return selectedKeys[0]
+}
+
